5249af95a759c243d18def31715e64f2a4c10889
Gilberto Diaz <diaz.gilberto@gmail.com>
2017-01-18 21:40:48 -0600

    adding comments

--- js/main.js
+++ js/main.js
@@ -1,80 +1,66 @@
-var charInput = document.getElementsByName('tree-char')[0];
-var heightInput = document.getElementsByName('tree-height')[0];
-var elements = {};
-
-
+var charInput = document.getElementsByName('tree-char')[0]
+var heightInput = document.getElementsByName('tree-height')[0]
+var elements = {}
 
 /**
- * Gets the height and character to 
- * print from the user inputs.
- */
-function getValues() {
-
+* @function getValues
+* @return {object} {height, character(e.i. #)}
+*/
+function getValues () {
     // Getting the values
-    var charValue = charInput.value;
-    var heightValue = heightInput.value;
+  var charValue = charInput.value
+  var heightValue = heightInput.value
 
     // Validating that variables have values.
-    if(heightValue && charValue) {
-        elements.currentHeight = heightValue;
-        elements.currentChar = charValue;
-    } else {
-        // Empty object and alerting the user.
-        alert('Both fields must have a value.');
-        elements = {};
-    }
-    return elements;
+  if (heightValue && charValue) {
+    elements.currentHeight = heightValue
+    elements.currentChar = charValue
+  } else {
+    // Empty object and alerting the user.
+    alert('Both fields must have a value.')
+    elements = {}
+  }
+  return elements
 }
 
-
-
 /**
- * Listening for the enter key.
- * 
- *  @param:     {event}
- */
-function onEnter(e) {
-    if(e.keyCode == 13) {
-        getValues();
-        growTree(elements);
-    }
+* @function onEnter
+* @param  {event} e {Listen for the enter key}
+* @return {call backs} {Runs getValues() and growTree()}
+*/
+function onEnter (e) {
+  if (e.keyCode === 13) {
+    getValues()
+    growTree(elements)
+  }
 }
 
-
-
-/**
- * Functions executes once the 
- * user clicks the botton.
- */
-function onClick() {
-    getValues();
-    growTree(elements);
+// Listen for the enter key
+function onClick () {
+  getValues()
+  growTree(elements)
 }
 
-
-
 /**
- * Function that prints to the console
- * character in a tree shape.
- * 
- *  @param:     {object}    {key: currentHeight, key: currentChar}
- *  @return:    {Prints to console currentChar in tree shape.}
- */
-function growTree(elements) {
-    var height = elements.currentHeight;
-    var symbol = elements.currentChar;
+* @function growTree
+* @param  {object} elements {height, character(e.i. #)}
+* @return {console.log()} {Prints to the console the tree.}
+*/
+function growTree (elements) {
+  var height = elements.currentHeight
+  var symbol = elements.currentChar
 
-    for (var i = 0; i < height; i++) {
-        var str1 = '';
-        var str2 = ''
-        for (var j = height - 1; j >= i; j--) {
-            str1 += ' ';
-            if (j == i) {
-                for(var z = (i*2)+1; z > 0; z--) {
-                    str2 += symbol;
-                }
-            }
+  for (var i = 0; i < height; i++) {
+    var str1 = ''
+    var str2 = ''
+    for (var j = height - 1; j >= i; j--) {
+      str1 += ' '
+      if (j === i) {
+        for (var z = (i * 2) + 1; z > 0; z--) {
+          str2 += symbol
         }
-        console.log(str1 + str2);
+      }
     }
+    console.log(str1 + str2)
+  }
 }
