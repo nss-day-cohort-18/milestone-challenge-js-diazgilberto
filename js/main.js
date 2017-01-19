244042059a8f35613a2c204f2cd1ae2799711d38
Gilberto Diaz <diaz.gilberto@gmail.com>
2017-01-18 21:08:42 -0600

    grow the tree exercise

--- js/main.js
+++ js/main.js
@@ -1,10 +1,69 @@
-var heightInput = document.getElementsByTagName('tree-height');
-var charInput = document.getElementsByName('tree-char');
+var charInput = document.getElementsByName('tree-char')[0];
+var heightInput = document.getElementsByName('tree-height')[0];
+var elements = {};
 
-var heightValue = heightInput.value;
 
 
-function growTree(height) {
+/**
+ * Gets the height and character to 
+ * print from the user inputs.
+ */
+function getValues() {
+
+    // Getting the values
+    var charValue = charInput.value;
+    var heightValue = heightInput.value;
+
+    // Validating that variables have values.
+    if(heightValue && charValue) {
+        elements.currentHeight = heightValue;
+        elements.currentChar = charValue;
+    } else {
+        // Empty object and alerting the user.
+        alert('Both fields must have a value.');
+        elements = {};
+    }
+    return elements;
+}
+
+
+
+/**
+ * Listening for the enter key.
+ * 
+ *  @param:     {event}
+ */
+function onEnter(e) {
+    if(e.keyCode == 13) {
+        getValues();
+        growTree(elements);
+    }
+}
+
+
+
+/**
+ * Functions executes once the 
+ * user clicks the botton.
+ */
+function onClick() {
+    getValues();
+    growTree(elements);
+}
+
+
+
+/**
+ * Function that prints to the console
+ * character in a tree shape.
+ * 
+ *  @param:     {object}    {key: currentHeight, key: currentChar}
+ *  @return:    {Prints to console currentChar in tree shape.}
+ */
+function growTree(elements) {
+    var height = elements.currentHeight;
+    var symbol = elements.currentChar;
+
     for (var i = 0; i < height; i++) {
         var str1 = '';
         var str2 = ''
@@ -12,11 +71,10 @@ function growTree(height) {
             str1 += ' ';
             if (j == i) {
                 for(var z = (i*2)+1; z > 0; z--) {
-                    str2 += '*';
+                    str2 += symbol;
                 }
             }
         }
         console.log(str1 + str2);
     }
 }
-growTree(7);
